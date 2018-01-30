eb185b650c75d485a4c0c33c14df1f8ba65558d6
vladisha <vladiks@gmail.com>
2018-01-30 15:56:10 +0200

    fff

---
+++ Preferencies.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 12:46:54 2018
+
+@author: vladis
+"""
+
+class Preferencies (object):
+        PATH_DATA_FOLDER  = "C:\\Users\\vladis\\finsim\\Data\\"
+        TEST_FOLDER  = "C:\\Users\\vladis\\finsim\\Test\\"
+    
